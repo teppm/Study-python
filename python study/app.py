11d77e7314a72e2a6d7c39d6befe9435cd9e822c
Tepp <Madis.Tepp@sas.se>
2020-02-04 20:47:15 +0100

    settings upd, bc img working

--- python study/app.py
+++ python study/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 
-def home():
+def index():
     return render_template("index.html")
 
 @app.route("/about")
