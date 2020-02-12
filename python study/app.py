41857ac923b6446cf1b90ecb237ee2e240d04157
Tepp <Madis.Tepp@sas.se>
2020-02-12 20:45:56 +0100

    form

--- python study/app.py
+++ python study/app.py
@@ -1,9 +1,11 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'whatintheholycuaqamole'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 
 @app.route("/")
 
@@ -30,10 +32,15 @@ def about_member(member_name):
 
     return render_template("member.html", member=member)
  
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form["name"]))
+  
     return render_template("contact.html", page_title="Contacts")
+
 
 
 @app.route("/careers")
