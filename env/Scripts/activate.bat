aba4cab26290a9f1b2d3158574e0a66f1d1cb147
unknown <madis.tepp@gmail.com>
2020-02-14 17:59:49 +0100

    env venv

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\13854\OneDrive - Scandinavian Airlines\Coding\Practical python\Study-python\env
+set VIRTUAL_ENV=C:\Users\meie\Desktop\Programming\Study-python\env
 
 if not defined PROMPT set PROMPT=$P$G
 
